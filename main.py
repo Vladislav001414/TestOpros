982b1b228fd6e7e8554db5808f9e3cb28972b560
Vladislav001414 <120846729+Vladislav001414@users.noreply.github.com>
2022-12-22 11:49:38 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,47 @@
+import socket
+
 from kivy.app import App
 from kivy.properties import ObjectProperty
-from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
+from kivy.uix.image import Image
 from kivy.core.window import Window
+from kivy.uix.floatlayout import FloatLayout
 
-
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(("192.168.0.173", 1234))
+client.send('User connected'.encode('utf-8'))
 
 
 #Window.size = (720,1280)
-Window.size =(480, 852)
-from kivy.config import Config
-Config.set('kivy', 'keyboard_mode', 'systemanddock')
+Window.size =(720, 1600)
+
 
 
 class Container(FloatLayout):
-     pass
+
+     def Login_user(self):
+
+
+             username_login = self.username.text
+             password_login = self.password.text
+             client.send('Username:'.encode('utf-8'))
+             client.send(username_login.encode('utf-8'))
+             client.send('\nPassword:'.encode('utf-8'))
+             client.send('\n'.encode('utf-8'))
+             client.send(password_login.encode('utf-8'))
 
 
 class MyTest(App):
     def build(self):
-        
-         return Container()
+
+        self.icon = 'icon.png'
+
+
+
+
+
+        return Container()
 
 
 if __name__ == '__main__':
